1d9255fb28037a7aa507495410207e64469a89f9
Ramin <RaminShamsi@yahoo.com>
2017-04-01 15:23:40 +0200

    add colore to that little bastrd circles

--- week3/day5/src/Circles.java
+++ week3/day5/src/Circles.java
@@ -6,24 +6,23 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Circles {
   public static void mainDraw(Graphics graphics) {
 
-//    graphics.drawOval(100, 100, 500, 500);
-    circle(graphics, 300, 300, 400);
-
+    circleRecursion(graphics, 300, 300, 400);
 
   }
 
-  public static void circle(Graphics graphics, int xCenter, int yCenter, int d) {
-
+  public static void circleRecursion(Graphics graphics, int xCenter, int yCenter, int d) {
 
     if (d < 10) {
 
     } else {
       graphics.drawOval(xCenter - d / 2, yCenter - d / 2, d, d); /// Drawing Circle with Java box corner as center
-
-      circle(graphics, xCenter, yCenter - d / 4, d / 2);   
-      circle(graphics, xCenter - (int) (d / 4 * Math.cos(Math.toRadians(30))),
+      graphics.setColor(Color.RED);
+      circleRecursion(graphics, xCenter, yCenter - d / 4, d / 2);
+      graphics.setColor(Color.BLUE);
+      circleRecursion(graphics, xCenter - (int) (d / 4 * Math.cos(Math.toRadians(30))),
               yCenter + (int) (d / 4 * Math.sin(Math.toRadians(30))), d / 2);
-      circle(graphics, xCenter + (int) (d / 4 * Math.cos(Math.toRadians(30))),
+      graphics.setColor(Color.orange);
+      circleRecursion(graphics, xCenter + (int) (d / 4 * Math.cos(Math.toRadians(30))),
               yCenter + (int) (d / 4 * Math.sin(Math.toRadians(30))), d / 2);
     }
   }
