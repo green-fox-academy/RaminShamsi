f0d373e4963755786a5ecfe169c02af4e409d14e
Ramin <RaminShamsi@yahoo.com>
2017-04-01 23:11:26 +0200

    add line drawing function with 2 return values

--- week3/day5/src/Tree.java
+++ week3/day5/src/Tree.java
@@ -1,66 +1,60 @@
-import javafx.util.Pair;
-
 import javax.swing.*;
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Tree {
-
-  public static void mainDraw(Graphics graphics) {
-    int xStart = 280;
-    int yStart = 500;
+  private static void mainDraw(Graphics graphics) {
+    int xStart = 700;
+    int yStart = 600;
     float alpha = 90;
-    int length = 30;
-    int numberOfIteration =10;
+    int length = 50;
+    int numberOfIteration = 10;
+
     graphics.setColor(Color.black);
-    graphics.fillRect(0, 0, 600, 600);
+    graphics.fillRect(0, 0, 1400, 750);
 
-    Branch(graphics, xStart, yStart, alpha,  length, numberOfIteration);
-
+    Branch(graphics, xStart, yStart, alpha, length, numberOfIteration);
   }
 
   private static void Branch(Graphics graphics, double xStart, double yStart,
-                             float alpha,  int length, int n) {
-
+                             float alpha, int length, int n) {
+/*
     graphics.setColor(new Color((float) Math.random(), (float) Math.random(), (float) Math.random()));
     double xEnd = xStart +  (length * Math.cos(Math.toRadians(alpha)));
     double yEnd = yStart -  (length * Math.sin(Math.toRadians(alpha)));
     graphics.drawLine((int) xStart, (int) yStart, (int) xEnd, (int) yEnd);
-
-//    makeLine <xEnd, yEnd>( graphics,  xStart,  yStart,  alpha,  length);
-//    System.out.println(xEnd);
+*/
+    double[] Ends = makeLine(graphics, xStart, yStart, alpha, length);
+    double xEnd = Ends[0];                                                 // reading back the return values
+    double yEnd = Ends[1];
 
     if (n > 0) {
-      Branch(graphics, xEnd, yEnd, alpha + 20, length, n-1);
-      Branch(graphics, xEnd, yEnd, alpha , length, n-1);
-      Branch(graphics, xEnd, yEnd, alpha - 20,  length, n-1);
-
+      Branch(graphics, xEnd, yEnd, alpha + 20, length, n - 1);
+      Branch(graphics, xEnd, yEnd, alpha, length, n - 1);
+      Branch(graphics, xEnd, yEnd, alpha - 20, length, n - 1);
     }
   }
 
-  private static Pair <Integer, Integer> makeLine(Graphics graphics, int xStart, int yStart,
-                                                  float alpha, int length) {
-
+  private static double[] makeLine(Graphics graphics, double xStart, double yStart,
+                                   float alpha, int length) {
+    double[] Ends = new double[2];
     graphics.setColor(new Color((float) Math.random(), (float) Math.random(), (float) Math.random()));
-    int xEnd = xStart + (int) (length * Math.cos(Math.toRadians(alpha)));
-    int yEnd = yStart - (int) (length * Math.sin(Math.toRadians(alpha)));
-    graphics.drawLine( xStart,  yStart,  xEnd,  yEnd);
-    return new Pair<>(xEnd, yEnd);
+    Ends[0] = xStart + (int) (length * Math.cos(Math.toRadians(alpha)));
+    Ends[1] = yStart - (int) (length * Math.sin(Math.toRadians(alpha)));
+    graphics.drawLine((int) xStart, (int) yStart, (int) Ends[0], (int) Ends[1]); // casting back to integer for drawing
+    return Ends;                          // Bringing back xEnds, yEnds in form of array
   }
 
-
   //    Don't touch the code below
-
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(600, 600));
+    jFrame.setSize(new Dimension(1400, 750));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-
   static class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
