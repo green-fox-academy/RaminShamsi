de94ed9f0c611dfa11136a871f00578500fb768c
Ramin <RaminShamsi@yahoo.com>
2017-04-03 13:11:39 +0200

    add the solved version of Domino

--- week4/day1/src/Dominoes.java
+++ week4/day1/src/Dominoes.java
@@ -8,16 +8,17 @@ public class Dominoes {
     // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
     // eg: [2, 4], [4, 3], [3, 5] ...
     List<Domino> modifiedVersion = new ArrayList<>();
-    for (int j = 0; j < dominoes.size() - 1; j++) {
-      for (int i = 0; i < dominoes.size() - 1; i++) {
-        int[] part1 = dominoes.get(j).getValues();
-        int[] part2 = dominoes.get(i + 1).getValues();
+    modifiedVersion.add(dominoes.get(0));             //put the 1st domino inside modified version
+    for (int j = 0; j < dominoes.size(); j++) {
+      int[] part1 = modifiedVersion.get(j).getValues(); //pick up the each element from modified version to compare
+      for (int i = 0; i < dominoes.size(); i++) {     //comparing each element of modified version with original one
+        int[] part2 = dominoes.get(i).getValues();
         if (part1[1] == part2[0]) {
-          modifiedVersion.add(dominoes.get(j));
-          modifiedVersion.add(dominoes.get(i + 1));
+          modifiedVersion.add(dominoes.get(i));
         }
       }
     }
+    System.out.println(dominoes);
     System.out.println(modifiedVersion);
   }
 
