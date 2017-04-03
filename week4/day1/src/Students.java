c99a0353f72d2a2ab41a9cd8bea81087a682e88b
Ramin <RaminShamsi@yahoo.com>
2017-04-03 16:42:07 +0200

    add Student-Teacher

---
+++ week4/day1/src/Students.java
@@ -0,0 +1,12 @@
+/**
+ * Created by HP on 4/3/2017.
+ */
+public class Students {
+
+  public void learn(){
+  }
+  public void question (Teachers generalTeacher){  // we declared a General Teacher variable which is just available
+    generalTeacher.answer();                        //in this method and can use other methods from other classes!
+  }
+
+}
---
+++ week4/day1/src/Teachers.java
@@ -0,0 +1,12 @@
+/**
+ * Created by HP on 4/3/2017.
+ */
+public class Teachers {
+
+  public void teach(Students generalStudent) {    // we declared a General Student variable which is just available
+    generalStudent.learn();                       //in this method and can use other methods from other classes!
+  }
+
+  public void answer() {
+  }
+}
