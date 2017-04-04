d8ac3784481a18622f792664f599b5e950a0a962
Ramin <RaminShamsi@yahoo.com>
2017-04-04 22:47:35 +0200

    addAircraft method added to carrier class

---
+++ week4/day2/src/aircraft_carrier/AircraftBattleApp.java
@@ -0,0 +1,11 @@
+package aircraft_carrier;
+
+/**
+ * Created by HP on 4/4/2017.
+ */
+public class AircraftBattleApp {
+  public static void main(String[] args) {
+    Carrier myCarrier = new Carrier();
+    myCarrier.addAircrafts("F16");
+  }
+}
--- week4/day2/src/aircraft_carrier/Carrier.java
+++ week4/day2/src/aircraft_carrier/Carrier.java
@@ -17,4 +17,20 @@ public class Carrier {
     myCarrier = new ArrayList<>();
   }
 
+  public void addAircrafts(String typeOfAircraft) {
+    System.out.println("What type of Aircraft would you like to add to your Carrier, \"F16\" or \"F35\"?");
+    if (typeOfAircraft == "F16") {
+      F16 newF16 = new F16();
+      myCarrier.add(newF16);
+    } else {
+      F35 newF35 = new F35();
+      myCarrier.add(newF35);
+    }
+  }
+public void getStatus(){
+    
+}
+  public void fight() {
+
+  }
 }
