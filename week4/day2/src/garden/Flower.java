558816efb5624f8b547e118f1388a4e120a6a51f
Ramin <RaminShamsi@yahoo.com>
2017-04-04 15:19:45 +0200

    start the garden

--- week4/day2/src/garden/Flower.java
+++ week4/day2/src/garden/Flower.java
@@ -4,5 +4,6 @@ package garden;
  * Created by HP on 4/4/2017.
  */
 public class Flower {
-
+  int numberOfFlowere;
+  String colorOfFlower;
 }
--- week4/day2/src/garden/Tree.java
+++ week4/day2/src/garden/Tree.java
@@ -4,4 +4,6 @@ package garden;
  * Created by HP on 4/4/2017.
  */
 public class Tree {
+  int numberOfTree;
+  String colorOfTree;
 }
