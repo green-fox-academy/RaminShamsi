76194e0010a675bfbb19259cedf21084d5dd2839
Ramin <RaminShamsi@yahoo.com>
2017-04-05 12:32:05 +0200

    add all the tests

--- week4/day3/Tests/SumTest.java
+++ week4/day3/Tests/SumTest.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,10 +10,40 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by HP on 4/5/2017.
  */
 class SumTest {
-  Sum myList = new Sum(1,2,3);
+
+  ArrayList<Integer> myList;
+  Sum mySum;
+
+  @BeforeEach
+  public void beforeEach() {
+    mySum = new Sum();
+  }
 
   @Test
-  public void sumTest(){
-    assertEquals(myList, myList.sumOfList());
+
+   public void sumTest1() {
+    myList= new ArrayList<>(Arrays.asList(1, 2, 3));
+    assertEquals(6, mySum.sumOfList(myList));
+  }
+
+  @Test
+
+  public void sumTest2() {
+    myList= new ArrayList<>(Arrays.asList(1, 2, 3,4));
+    assertEquals(10, mySum.sumOfList(myList));
+  }
+
+  @Test
+// empty ArrayList
+  public void sumTestEmpty() {
+    myList= new ArrayList<>();
+    assertEquals(0, mySum.sumOfList(myList));
+     }
+
+  @Test
+// No Object
+  public void sumTestNull() {
+    myList = null;
+    assertEquals(0, mySum.sumOfList(myList));
   }
 }
--- week4/day3/src/Sum.java
+++ week4/day3/src/Sum.java
@@ -5,20 +5,16 @@ import java.util.List;
  * Created by HP on 4/5/2017.
  */
 public class Sum {
-  List<Integer> list;
 
-  public Sum(int a, int b, int c){
-    list = new ArrayList<>();
-    list.add(a);
-    list.add(b);
-    list.add(c);
-  }
 
-  public int sumOfList(){
+  public int sumOfList(ArrayList<Integer> myList){
     int sum =0;
-    for (int i = 0; i < list.size(); i++) {
-      sum +=list.get(i);
-
+    if(myList == null){
+      System.out.println("It is Null");
+      return 0;
+    }
+    for (int i = 0; i < myList.size(); i++) {
+      sum +=myList.get(i);
     }
     return sum;
   }
