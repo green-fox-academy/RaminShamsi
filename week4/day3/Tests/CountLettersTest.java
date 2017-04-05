1016f02201cac6c78ff701e1730e195e8c1d0430
Ramin <RaminShamsi@yahoo.com>
2017-04-05 13:59:12 +0200

    add CountLetterTest

---
+++ week4/day3/Tests/CountLettersTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by HP on 4/5/2017.
+ */
+class CountLettersTest {
+  @Test
+  public void dictionaryTest() {
+  CountLetters myObject = new CountLetters();
+  String test = "ababbaa";
+    HashMap<Character, Integer> testHasMap = new HashMap<>();
+    testHasMap.put('a',4);
+    testHasMap.put('b',3);
+
+  assertEquals(testHasMap, myObject.myDictionary(test));
+  }
+}
