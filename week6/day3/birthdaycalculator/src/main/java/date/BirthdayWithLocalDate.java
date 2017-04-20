e08c111993dc1ea86c6540100990ab6452e91f3d
Ramin <RaminShamsi@yahoo.com>
2017-04-20 16:18:44 +0200

    add birthday calculator

--- week6/day3/birthdaycalculator/src/main/java/date/BirthdayWithLocalDate.java
+++ week6/day3/birthdaycalculator/src/main/java/date/BirthdayWithLocalDate.java
@@ -1,35 +1,59 @@
 package date;
 
+import sun.util.resources.cldr.vai.LocaleNames_vai;
+
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 import java.util.Scanner;
+
 
 public class BirthdayWithLocalDate implements BirthdayCalculator<LocalDate> {
 
   @Override
   public LocalDate parseDate(String str) {
     // TODO - return with the parsed date; format is: yyyy-MM-dd
+     return LocalDate.parse(str);
   }
 
   @Override
   public String printMonthAndDay(LocalDate date) {
-    // TODO - return the date formatted: month & day (MM. dd.)
+    // TODO - return the date formatted: month & day ()
+    //   String dd=String.valueOf(date.getDayOfMonth());
+    //  String mm=String.valueOf(date.getMonth());
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM. dd.");
+    return date.format(formatter);
   }
 
   @Override
   public boolean isAnniversaryToday(LocalDate date) {
     // TODO - return with true if today is the same month+day as date
+    LocalDate now = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM. dd.");
+    if (date.format(formatter).equals(now.format(formatter))) {
+      return true;
+    }
+    return false;
   }
 
   @Override
   public int calculateAgeInYears(LocalDate birthday) {
     // TODO - return how many years age the input date 'birthday' was
+    LocalDate now = LocalDate.now();
+    return now.getYear() - birthday.getYear();
   }
 
   @Override
   public int calculateDaysToNextAnniversary(LocalDate date) {
     // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
+    LocalDate now = LocalDate.now();
+    if (now.getDayOfYear() > date.getDayOfYear()) {
+      return LocalDate.of(now.getYear(), 12, 31).getDayOfYear() - now.getDayOfYear() + date.getDayOfYear();
+    } else {
+      return date.getDayOfYear() - now.getDayOfYear();
+    }
   }
 
   public static void main(String[] args) {
