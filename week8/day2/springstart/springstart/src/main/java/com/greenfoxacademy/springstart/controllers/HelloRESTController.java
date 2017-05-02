45be8c0fb6fac677476d2924a85ea4f4c5a6e90a
Ramin <RaminShamsi@yahoo.com>
2017-05-02 15:14:30 +0200

    add counter

--- week8/day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week8/day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,14 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 
 @RestController
 public class HelloRESTController {
 
+  AtomicLong counter = new AtomicLong();
+
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam("anyName") String name) {
     // the URL should be like: http://localhost:8080/greeting?anyName=Ramin
-    Greeting myGreeting = new Greeting(1, "Hello, " + name + "!");
+    Greeting myGreeting = new Greeting(counter.incrementAndGet(), "Hello, " + name + "!");
     return myGreeting;
   }
 }
