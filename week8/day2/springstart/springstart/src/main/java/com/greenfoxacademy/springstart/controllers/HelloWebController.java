58851f1e843b6625e6ed4b1b029f503cdc9dbbe0
Ramin <RaminShamsi@yahoo.com>
2017-05-02 16:02:06 +0200

    add counter to WebController with still greeting to a fixed name

--- week8/day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week8/day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloWebController {
 
+  AtomicLong newCounter = new AtomicLong();
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model ) {
+    model.addAttribute("nameOfAnything", "Ramin");
+    model.addAttribute("counter", newCounter.incrementAndGet());
     return "greeting";
   }
 }
