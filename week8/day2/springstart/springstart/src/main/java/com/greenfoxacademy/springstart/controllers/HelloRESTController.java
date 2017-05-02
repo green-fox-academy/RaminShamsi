71212b7328414d1e8b2bb61cd3dcc0c4f614962c
Ramin <RaminShamsi@yahoo.com>
2017-05-02 13:59:30 +0200

    it is now greeting with giving special name in URL

--- week8/day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week8/day2/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRESTController {
 
   @RequestMapping(value = "/greeting")
-  public Greeting greeting(){
-    Greeting myGreeting = new Greeting(1, "Hollo world!");
-    return  myGreeting;
+  public Greeting greeting(@RequestParam("anyName") String name) {
+    // the URL should be like: http://localhost:8080/greeting?anyName=Ramin
+    Greeting myGreeting = new Greeting(1, "Hello, " + name + "!");
+    return myGreeting;
   }
 }
