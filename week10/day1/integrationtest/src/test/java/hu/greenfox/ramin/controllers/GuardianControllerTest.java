92c6a2746e90b8acaad64e6ce8faf24d2cc2338e
Ramin <RaminShamsi@yahoo.com>
2017-05-15 11:52:44 +0200

    add the tests for testing responds and error

---
+++ week10/day1/integrationtest/src/test/java/hu/greenfox/ramin/controllers/GuardianControllerTest.java
@@ -0,0 +1,60 @@
+package hu.greenfox.ramin.controllers;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.jayway.jsonpath.JsonPath;
+import hu.greenfox.ramin.IntegrationtestApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = IntegrationtestApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
+public class GuardianControllerTest {
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void yourFirstTest() throws Exception {
+    mockMvc.perform(get("/groot")
+            .contentType(MediaType.APPLICATION_JSON)
+            .param("message","Hello"))                         /// End of the Get Method  //Giving the ?message=Hello
+            .andExpect(status().isOk())                       // Starting of perform
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.translated", is("I am Groot!")));
+
+
+  }
+}
