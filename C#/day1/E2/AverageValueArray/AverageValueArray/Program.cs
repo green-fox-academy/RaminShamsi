26646a0e9f0fe2c089f6d1d4204a6abb0355fac1
Ramin <RaminShamsi@yahoo.com>
2017-07-03 12:02:16 +0200

    add the 2nd exercise

---
+++ C#/day1/E2/AverageValueArray/AverageValueArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageValueArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sampleArray = new int[] { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14};
+            double averageOfOdds = sampleArray.Where(n => n % 2 != 0).Average(n => n);
+            Console.WriteLine(averageOfOdds);
+            Console.ReadKey();
+        }
+    }
+}
