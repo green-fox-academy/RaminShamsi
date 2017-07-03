51846427b250a5dd96b61617d204963f10880705
Ramin <RaminShamsi@yahoo.com>
2017-07-03 13:26:34 +0200

    add squared positive values

---
+++ C#/day1/E3/SquaredValueOfPositives/SquaredValueOfPositives/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredValueOfPositives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sampleArray = new int[] { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+           
+            IEnumerable<int> positiveElements = sampleArray.Where(n => n >= 0);
+            var sq = from int element in positiveElements
+                     let squared = element * element
+                     select new {squared};
+            foreach(double item in positiveElements)
+                Console.WriteLine(Math.Pow(item, 2));
+            foreach (var item in sq)
+            Console.WriteLine(item);
+            Console.ReadKey();
+        }
+    }
+}
