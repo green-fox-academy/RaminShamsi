79b03c127fb139ce40026fb06e83e1f92bed5731
Ramin <RaminShamsi@yahoo.com>
2017-07-03 15:07:13 +0200

    add the displayBoard method

---
+++ C#/day1/BattleShip/BattleShip/BattleShip/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+
+    class BattleShipBoard
+    {
+        public void DisplayBoard()
+        {
+            Console.WriteLine("    A B C D E F G H I J K L M N O P Q R S T");
+            for (int i = 1; i <= 20; i++)
+            {
+                if(i <= 9)
+                {
+                    Console.Write(" " + i + ":");
+                } else
+                {
+                    Console.Write(i + ":");
+                }
+                Console.Write(" . . . . . . . . . . . . . . . . . . . .\n");
+               
+            }
+        }
+    }
+
+    class Player
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BattleShipBoard b = new BattleShipBoard();
+            b.DisplayBoard();
+            Console.ReadKey();
+        }
+    }
+}
