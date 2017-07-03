a7d547bc8d2deeb198a153bfb164a61095183ecf
Ramin <RaminShamsi@yahoo.com>
2017-07-03 11:01:11 +0200

    add the even number in array exrcise

---
+++ C#/day1/E1/EvenNumberFromArray/EvenNumberFromArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumberFromArray
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> evenNumbers = myArray.Where(n => n % 2 == 0);
+            Console.WriteLine("All even numbers: " + String.Join(",", evenNumbers));
+            IEnumerable<int> positiveEvenNumbers = myArray.Where(n => n % 2 == 0 && n>=0);
+            Console.WriteLine("All positive even numbers: " + String.Join(",", positiveEvenNumbers));
+            Console.ReadKey();
+        }
+    }
+}
