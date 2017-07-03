73eca8741d9a98a075b6b2260c1b7f7e3d0e33d4
Ramin <RaminShamsi@yahoo.com>
2017-07-03 17:51:29 +0200

    generate random ship place

--- C#/day1/BattleShip/BattleShip/BattleShip/Program.cs
+++ C#/day1/BattleShip/BattleShip/BattleShip/Program.cs
@@ -9,36 +9,94 @@ namespace BattleShip
 
     class BattleShipBoard
     {
-        public void DisplayBoard()
+        public void DisplayBoard(char[,] grid)
         {
             Console.WriteLine("    A B C D E F G H I J K L M N O P Q R S T");
             for (int i = 1; i <= 20; i++)
             {
-                if(i <= 9)
+                if (i <= 9)
                 {
                     Console.Write(" " + i + ":");
-                } else
+                }
+                else
                 {
                     Console.Write(i + ":");
                 }
-                Console.Write(" . . . . . . . . . . . . . . . . . . . .\n");
-               
+                for (int j = 1; j <= 20; j++)
+                {
+                    Console.Write(" " + grid[i - 1, j - 1]);
+
+                }
+                Console.WriteLine("");
             }
         }
     }
 
     class Player
     {
+        char[,] Grid = new char[20, 20];
+        public int HitCount = 0;
+        public int MissCount = 0;
+        int x = 0;
+        int y = 0;
 
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
+        public int getHitCount()
         {
-            BattleShipBoard b = new BattleShipBoard();
-            b.DisplayBoard();
-            Console.ReadKey();
+            return HitCount;
+        }
+        public int getMissCount()
+        {
+            return MissCount;
+        }
+        public char[,] GetGrid()
+        {
+            return Grid;
+        }
+        public void SetGrid()
+        {
+           
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    Grid[x, y] = '.';
+                }
+            }
+            for (int i = 1; i <= 13; i++)
+                {
+                x = RandomNumber();
+                y = RandomNumber();
+                while (Grid[x,y] == 'S')
+                {
+                    x = RandomNumber();
+                    y = RandomNumber(); 
+                }    
+                Grid[x, y] = 'S';
+                Console.WriteLine(x + y + Grid[x,y]);
+                }
+        }
+
+        public int RandomNumber()
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            return r.Next(1, 20);
+        }
+
+        public void AskCoordinates()
+        {
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Player p = new Player();
+                BattleShipBoard b = new BattleShipBoard();
+                p.SetGrid();
+                b.DisplayBoard(p.GetGrid());
+
+                Console.ReadKey();
+            }
         }
     }
 }
