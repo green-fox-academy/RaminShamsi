520193320eac2fc18ae6b3de05edc57b032636ab
Ramin <RaminShamsi@yahoo.com>
2017-07-05 18:23:48 +0200

    cleaned the code

--- week4/day3/src/Fruit.java
+++ week4/day3/src/Fruit.java
@@ -1,13 +1,11 @@
-/**
- * Created by HP on 4/5/2017.
- */
+
 public class Fruit {
   String fruitName;
 
   public Fruit(String nameOfFruit){
     this.fruitName = nameOfFruit;
   }
-  public String getApple (){
+  public String getApple(){
     return fruitName;
   }
 }
